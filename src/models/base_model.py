386a3662cd6302f338c2efb6353420a0d068ee51
naz2001r <naz2001r@gmail.com>
2023-06-28 16:06:48 +0300

    Added docsting to base model

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -4,21 +4,42 @@ import time
 
 
 class BaseModel:
+    """Base class for all models."""
     BASE_PATH: str = 'data\\models'
 
     def __init__(self, method_name: str):
+        """"
+        Initialize the BaseModel.
+        
+        Args:
+            method_name (str): The name of the method used for the model.
+        """
         self._method_name = method_name
 
     def predict(self) -> None:
+        """Generate predictions for the given users."""
         raise NotImplementedError()
     
     def train(self) -> None:
+        """Train the model."""
         raise NotImplementedError()
     
     def load(self, file_name: str) -> object:
+        """
+        Load a model from a file.
+        
+        Args:
+            file_name (str): The name of the file to load the model from.
+        """
         return pickle.load(open(file_name, 'rb'))
     
     def dump(self, file_name: str) -> None:
+        """
+        Dump the model to a file.
+
+        Args:
+            file_name (str): The name of the file to dump the model to.
+        """
         os.makedirs(os.path.join(self.BASE_PATH, self._method_name), exist_ok=True)
         time_stamp = time.time_ns()
         pickle.dump(self, open(os.path.join(self.BASE_PATH, self._method_name, file_name + f'_{time_stamp}.pkl'), 'wb'))
