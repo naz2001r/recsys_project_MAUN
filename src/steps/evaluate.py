d0a78bb7c8e742bbe1fc4da2d8b90b732d2467a4
oleksandr.vashchuk <oleksandr.vashchuk@justanswer.com>
2023-07-15 17:39:47 +0300

    rewrited pipeline

--- src/steps/evaluate.py
+++ src/steps/evaluate.py
@@ -13,10 +13,10 @@ from base_model import BaseModel
 from metrics import ReccomenderMetrics
 import dvc.api
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     sys.stderr.write('Arguments error. Usage:\n')
     sys.stderr.write(
-        '\tpython3 evaluate.py eval_file full_file\n'
+        '\tpython3 evaluate.py eval_file full_file model_name\n'
     )
     sys.exit(1)
 
@@ -31,35 +31,39 @@ eval_df = pd.read_csv(eval_file)
 file_full = sys.argv[2]
 df_full = pd.read_csv(file_full)
 
+model_name = sys.argv[3]
+
 model_dump = ''
 
-if len(sys.argv) == 4:
-    model_dump = sys.argv[3]
+if len(sys.argv) == 5:
+    model_dump = sys.argv[4]
 else:
-    list_of_files = glob.glob('./data/models/**/*.pkl', recursive=True)
+    list_of_files = glob.glob('./data/models/{model_name}/*.pkl', recursive=True)
     print('Next models will be evaluated:')
     print(list_of_files)
+
+model_dump = list_of_files[0]
 
 metrics_obj = ReccomenderMetrics()
 metrics = {}
 
-for model_dump in list_of_files:
-    print(f'Model evaluation started {model_dump}')
-    eval_model = BaseModel('base').load(model_dump)
-    metrics[f'{eval_model.name()}'] = {}
+print(f'Model evaluation started {model_dump}')
+eval_model = BaseModel('base').load(model_dump)
+metrics[f'{eval_model.name()}'] = {}
     
-    users = eval_df[user_column].unique()
-    pred = eval_model.predict(users, k=max(k_list))
-    true = [eval_df[eval_df[user_column] == user_id].sort_values(by="Book-Rating", ascending=False)[book_column].tolist() for user_id in users]
+users = eval_df[user_column].unique()
+pred = eval_model.predict(users, k=max(k_list))
+true = [eval_df[eval_df[user_column] == user_id].sort_values(by="Book-Rating", ascending=False)[book_column].tolist() for user_id in users]
 
-    for k in k_list:
-        metrics[f'{eval_model.name()}'][f'map@{k}'] = metrics_obj.mapk(true, pred, k)
-        metrics[f'{eval_model.name()}'][f'precision@{k}'] = np.mean([metrics_obj.precisionk(true[i][:k], pred[i][:k], k) for i in range(len(users))])
-        metrics[f'{eval_model.name()}'][f'recall@{k}'] = np.mean([metrics_obj.recallk(true[i][:k], pred[i][:k], k) for i in range(len(users))])
-        metrics[f'{eval_model.name()}'][f'ndcg@{k}'] = np.mean([metrics_obj.ndcgk(true[i][:k], pred[i][:k], k) for i in range(len(users))])
+for k in k_list:
+    metrics[f'{eval_model.name()}'][f'map@{k}'] = metrics_obj.mapk(true, pred, k)
+    metrics[f'{eval_model.name()}'][f'precision@{k}'] = np.mean([metrics_obj.precisionk(true[i][:k], pred[i][:k], k) for i in range(len(users))])
+    metrics[f'{eval_model.name()}'][f'recall@{k}'] = np.mean([metrics_obj.recallk(true[i][:k], pred[i][:k], k) for i in range(len(users))])
+    metrics[f'{eval_model.name()}'][f'ndcg@{k}'] = np.mean([metrics_obj.ndcgk(true[i][:k], pred[i][:k], k) for i in range(len(users))])
     
-    metrics[f'{eval_model.name()}'][f'coverage'] = metrics_obj.coverage(true, df_full["ISBN"].unique())
+metrics[f'{eval_model.name()}'][f'coverage'] = metrics_obj.coverage(true, df_full["ISBN"].unique())    
 
 os.makedirs('data/eval', exist_ok=True)
-with open(os.path.join('data/eval', "metrics.json"), "w") as mf:
+metrics_file = f'metrics_{model_name}.json'
+with open(os.path.join('data/eval', metrics_file), "w") as mf:
     json.dump(metrics, mf)
